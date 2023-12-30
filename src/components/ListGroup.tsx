aacacee05d796d1bfafcad0c9d9a8c4c4865761c
James-wasonga <jameswasonga22@gmail.com>
2023-12-30 12:32:47 -0800

    update new

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,3 +1,6 @@
+import { MouseEvent} from "react";
+import { useState } from "react";
+
 function ListGroup () {
 
     let items = [
@@ -7,19 +10,34 @@ function ListGroup () {
         'Norway',
         'Cannada'
     ]
+ //items = []
 
+//type a notation
+ const handleClick = (event:MouseEvent) => console.log(event); 
+ const [selectedIndex,setSelectedIndex] = useState(-1); 
   
     return (
         <>
             <h1> List </h1>
-            {items.length === 0 ? <p>No item found</p>:null}
+         {items.length === 0 ? <p>No item found </p>: null}
             {items.length === 0 && <p>No item found </p>}
             <ul className ="list-group">
-            {items.map(item => <li className="list-group-item"  onClick={() => console.log(item)}> {item}</li>)}
+            {items.map(item =>
+                 <li className="list-group-item active"  onClick={handleClick}> {item}</li>)} 
 
         </ul>
         </>
     );
 }
-
+ 
 export default ListGroup;
+
+
+
+// //how query is done in java script
+
+// const btn = document.querySelector('.btn');
+// btn.addEventListener('click', () => {
+// const div = document.querySelector('#div');
+// div.style.display = 'none';
+// });
