3d2af220a75ca99a68cfddafdfedb98c9dd83515
James-wasonga <jameswasonga22@gmail.com>
2024-01-05 13:08:43 -0800

    commit

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 interface Props{
     children: string;
-    color?: 'primary' | 'secondary' ;
+    color?: 'primary' | 'secondary' | 'danger';
     onClick: () => void;
 }
 
