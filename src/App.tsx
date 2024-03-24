22e50ed5a40745ebd93ceda693c07525bc44a2e2
James-wasonga <jameswasonga22@gmail.com>
2024-03-24 21:57:38 +0300

    upd on array-items

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,44 @@
-// //import message file
-
-// import ListGroup from "./components/ListGroup";
-
-// function App () {
-//     let items = [
-//         'Amerca',
-//         'Landon',
-//         'France',
-//         'Norway',
-//         'Cannada'
-//     ]
-//     const handleSelectItem = (item: string) => {
-//         console.log(item);
-//     }
-// return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/></div>
-
-// }
-
-// export default App;
-
 //import message file
+
+import ListGroup from "./components/ListGroup";
+
+function App () {
+    let items = [
+        'Amerca',
+        'Landon',
+        'France',
+        'Norway',
+        'Cannada'
+    ]
+    const handleSelectItem = (item: string) => {
+        console.log(item);
+    }
+return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/></div>
+
+}
+
+export default App;
+
+import message file
 
  
 
 
-//import Alert from "./components/ListGroup";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
+// //import Alert from "./components/ListGroup";
+// import Alert from "./components/Alert";
+// import Button from "./components/Button";
 
-    function App() {
+//     function App() {
         
-return (
-<div>
-    {/* <Alert> 
-    Hello <span> world </span>
-    </Alert>  */}
+// return (
+// <div>
+//     {/* <Alert> 
+//     Hello <span> world </span>
+//     </Alert>  */}
     
-    <Button color="primary" onClick ={() => console.log('clicked')}>Button</Button>
-</div>
-); 
-}
+//     <Button color="primary" onClick ={() => console.log('clicked')}>Button</Button>
+// </div>
+// ); 
+// }
 
-export default App;
+// export default App;
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,6 +10,6 @@ const Button =({children, onClick, color='primary'}:Props) => {
   return (
     <button className={'btn btn-' + color} onClick ={() => console.log('clicked')} >Button</button>
   )
-}
+} 
 
 export default Button 
